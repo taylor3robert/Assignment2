61d20af08e93dd0aa51597c60b2207a1f2682832
Robert Taylor <taylor3robert@gmail.com>
2025-04-09 11:39:51 -0400

    added javascript

---
+++ index.js
@@ -0,0 +1,20 @@
+let person = {
+    name: "Robert Taylor",
+    age: "41",
+    bootcamp: "Software Engineering"
+}
+
+let students = [
+    { name: "Mary Jones",
+      age: 23,
+      isEnrolled: false  
+    },
+    { name: "Jeff Jones",
+      age: 34,
+      isEnrolled: true   
+    },
+    { name: "Donna Hodge",
+      age: 56,
+      isEnrolled: false  
+    }
+]
